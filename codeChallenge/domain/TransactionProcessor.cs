664ccff2a8ad50a3d70bf6f1a1b86c3c5e5a0550
ajeet kumar <ajeetx@email.com>
2020-09-05 16:35:07 +1000

    refactored

--- codeChallenge/domain/TransactionProcessor.cs
+++ codeChallenge/domain/TransactionProcessor.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using System;
 namespace codeChallenge
 {
     public interface ITransactionProcessor
@@ -15,22 +14,19 @@ namespace codeChallenge
             _inputFormatter=inputFormatter;
         }
         public Output ExtractTransaction(IEnumerable<Transaction> transactions,string[] args)
-        {
-            Output output=new Output{};
-            
+        {           
             var input=_inputFormatter.Cast2Type(args);
 
-            var reversalTransactions=transactions.Where(txn=>txn.TransactionType.ToUpperInvariant()=="REVERSAL");           
+            var reversalTransactions=transactions.Where(txn=>txn.TransactionType.ToLowerInvariant()=="reversal");           
             
             var result=transactions.Where(txn=>
                 !reversalTransactions.FirstOrDefault().RelatedTransaction.Contains(txn.TransactionId) && 
                  txn.FromAccountId==input.AccountId && txn.CreatedAt>=input.From && txn.CreatedAt<=input.To);
             
-            foreach(var r in result)
-            {
-                output.RelativeBalance-=r.Amount;
-                output.NoOfTransaction++;
-            }
+            var output=new Output{
+                RelativeBalance=result.Sum(r=>-r.Amount),
+                NoOfTransaction=result.Count()
+            };
             
             return output;;
         }
